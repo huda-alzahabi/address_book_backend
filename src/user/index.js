ddd2fa11421312ac1d424f7757c5a2b6a769dd9e
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-24 13:36:16 +0300

    Created the user routes

---
+++ src/user/index.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const { get, register, login } = require("./controller/user");
+const router = Router();
+const userMiddleware = require("../../middleware/user_middleware");
+
+router.get("/", userMiddleware(), (req, res) => get(req, res));
+router.post("/auth/register", register);
+router.post("/auth/login", login);
+
+module.exports = router;
